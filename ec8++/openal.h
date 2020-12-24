b8e2990fe458eda040ee9af11118dd2a4e859106
Nikita Serba <nikitaserba@icloud.com>
2020-12-24 20:17:27 +0200

    Add OpenAL
    Signed-off-by: Nikita Serba <nikitaserba@icloud.com>

---
+++ ec8++/openal.h
@@ -0,0 +1,47 @@
+/**
+ *  _____ _____  _____
+ * |  ___/  __ \|  _  | _     _
+ * | |__ | /  \/ \ V /_| |_ _| |_
+ * |  __|| |     / _ \_   _|_   _|
+ * | |___| \__/\| |_| ||_|   |_|
+ * \____/ \____/\_____/
+ *
+ * Simple C++ CHIP-8 emulator (Windows/macOS/Linux/Unix).
+ * Copyright (C) 2020 Nikita Serba. All rights reserved
+ * https://github.com/sandsbit/ec8pp/
+ *
+ *  This file is part of ec8pp.
+ *
+ *  ec8pp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  ec8pp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with ec8pp.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef EC8_OPENAL_H
+#define EC8_OPENAL_H
+
+#ifdef __APPLE__
+
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
+#include <OpenAL/alut.h>
+
+#else
+
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
+
+#endif
+
+#endif //EC8_OPENAL_H
