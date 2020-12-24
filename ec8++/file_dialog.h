223e32191c6503a11ef6cdb69446b1b8b671d2c6
Nikita Serba <nikitaserba@icloud.com>
2020-12-24 22:31:10 +0200

    Add function that opens file open dialog (no impl)
    Signed-off-by: Nikita Serba <nikitaserba@icloud.com>

---
+++ ec8++/file_dialog.h
@@ -0,0 +1,48 @@
+/**
+ *  _____ _____  _____
+ * |  ___/  __ \|  _  | _     _
+ * | |__ | /  \/ \ V /_| |_ _| |_
+ * |  __|| |     / _ \_   _|_   _|
+ * | |___| \__/\| |_| ||_|   |_|
+ * \____/ \____/\_____/
+ *
+ * Simple C++ CHIP-8 emulator (Windows/macOS/Linux/Unix).
+ * Copyright (C) 2020 Nikita Serba. All rights reserved
+ * https://github.com/sandsbit/ec8pp/
+ *
+ *  This file is part of ec8pp.
+ *
+ *  ec8pp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  ec8pp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with ec8pp.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef EC8_FILE_DIALOG_H
+#define EC8_FILE_DIALOG_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Open file open dialog and return selected file.
+ *
+ * @return pointer to c-string with file path allocated with malloc() (don't forget to use free())
+ */
+char *openFileDialog(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //EC8_FILE_DIALOG_H
