65b6c570fd793e54f275fc388b3f2b175ebf0a57
Nikita Serba <nikitaserba@icloud.com>
2020-12-25 22:45:32 +0200

    Add input class (no impl)
    Signed-off-by: Nikita Serba <nikitaserba@icloud.com>

---
+++ ec8++/input.cpp
@@ -0,0 +1,32 @@
+/**
+ *  _____ _____  _____
+ * |  ___/  __ \|  _  | _     _
+ * | |__ | /  \/ \ V /_| |_ _| |_
+ * |  __|| |     / _ \_   _|_   _|
+ * | |___| \__/\| |_| ||_|   |_|
+ * \____/ \____/\_____/
+ *
+ * Simple C++ CHIP-8 emulator (Windows/macOS/Linux/Unix).
+ * Copyright (C) 2020 Nikita Serba. All rights reserved
+ * https://github.com/sandsbit/ec8pp/
+ *
+ *  This file is part of ec8pp.
+ *
+ *  ec8pp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  ec8pp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with ec8pp.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#include "input.h"
+
+#include "SDL2/SDL.h"
---
+++ ec8++/input.h
@@ -0,0 +1,74 @@
+/**
+ *  _____ _____  _____
+ * |  ___/  __ \|  _  | _     _
+ * | |__ | /  \/ \ V /_| |_ _| |_
+ * |  __|| |     / _ \_   _|_   _|
+ * | |___| \__/\| |_| ||_|   |_|
+ * \____/ \____/\_____/
+ *
+ * Simple C++ CHIP-8 emulator (Windows/macOS/Linux/Unix).
+ * Copyright (C) 2020 Nikita Serba. All rights reserved
+ * https://github.com/sandsbit/ec8pp/
+ *
+ *  This file is part of ec8pp.
+ *
+ *  ec8pp is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  ec8pp is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  and GNU Lesser General Public License along with ec8pp.  If not,
+ *  see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef EC8_INPUT_H
+#define EC8_INPUT_H
+
+enum InputKeys {
+    _0,
+    _1,
+    _2,
+    _4,
+    _5,
+    _6,
+    _7,
+    _8,
+    _9,
+    A,
+    B,
+    C,
+    D,
+    E,
+    F
+};
+
+class Input final {
+
+public:
+
+    static Input& getInstance();
+
+    Input(const Input &) = delete;
+    Input(Input &&) = delete;
+
+    Input& operator=(const Input &) = delete;
+    Input& operator=(Input &&) = delete;
+
+    [[nodiscard]] bool isKeyPressed(InputKeys key) const;
+    [[nodiscard]] InputKeys waitUntilKeyPress(InputKeys key) const;
+
+private:
+
+    Input();
+    ~Input();
+
+};
+
+
+#endif //EC8_INPUT_H
