ea5254ec631e5d330c91687fd2d80a73e6fcc75b
Nikita Serba <nikitaserba@icloud.com>
2020-12-25 23:14:56 +0200

    Add key bindings
    Signed-off-by: Nikita Serba <nikitaserba@icloud.com>

--- ec8++/input.h
+++ ec8++/input.h
@@ -31,13 +31,55 @@
 #define EC8_INPUT_H
 
 #include <cstdint>
+#include <stdexcept>
 
-#ifdef __cpp_consteval
-consteval
+#include "SDL2/SDL.h"
+
+// TODO: Remove when Apple Clang add c++20 support
+#if __has_cpp_attribute(likely)
+#define LIKELY [[likely]]
 #else
-constexpr
+#define LIKELY
 #endif
-inline auto getKeyBinding(std::uint8_t key);
+
+constexpr inline auto getKeyBinding(std::uint8_t key) {
+    switch (key) {
+        case 0x0:
+            return SDL_KeyCode::SDLK_KP_0;
+        case 0x1:
+            return SDL_KeyCode::SDLK_KP_7;
+        LIKELY case 0x2:
+            return SDL_KeyCode::SDLK_KP_8;
+        case 0x3:
+            return SDL_KeyCode::SDLK_KP_9;
+        LIKELY case 0x4:
+            return SDL_KeyCode::SDLK_KP_4;
+        case 0x5:
+            return SDL_KeyCode::SDLK_KP_5;
+        LIKELY case 0x6:
+            return SDL_KeyCode::SDLK_KP_6;
+        case 0x7:
+            return SDL_KeyCode::SDLK_KP_1;
+        LIKELY case 0x8:
+            return SDL_KeyCode::SDLK_KP_2;
+        case 0x9:
+            return SDL_KeyCode::SDLK_KP_3;
+        case 0xA:
+            return SDL_KeyCode::SDLK_KP_PERIOD;
+        case 0xB:
+            return SDL_KeyCode::SDLK_KP_ENTER;
+        case 0xC:
+            return SDL_KeyCode::SDLK_KP_DIVIDE;
+        case 0xD:
+            return SDL_KeyCode::SDLK_KP_MULTIPLY;
+        case 0xE:
+            return SDL_KeyCode::SDLK_KP_MINUS;
+        case 0xF:
+            return SDL_KeyCode::SDLK_KP_PLUS;
+        default:
+            throw std::runtime_error("Invalid key!");
+    }
+}
 
 class Input final {
 
